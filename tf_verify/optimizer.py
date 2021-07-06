8e7c3e42c4319e3b52d8c2d3506daec67ea04a99
Mark Mueller <mark.mueller@inf.ethz.ch>
2021-07-06 08:26:45 +0200

    fix typo

--- tf_verify/optimizer.py
+++ tf_verify/optimizer.py
@@ -70,7 +70,7 @@ class Optimizer:
                     matrix, m_input_names , output_name , b_output_shape  = self.resources[i][domain]
                     
                     bias_length = reduce((lambda x, y: x*y), b_output_shape)
-                    bias = nn.zeros(bias_length)
+                    bias = np.zeros(bias_length)
 
                     i += 1
                 if domain == 'deepzono':
@@ -102,7 +102,7 @@ class Optimizer:
                 else:
                     filters, image_shape, strides, pad_top, pad_left, pad_bottom, pad_right, c_input_names, b_output_name, b_output_shape = self.resources[i][domain]
                     bias_length = reduce((lambda x, y: x*y), output_shape)
-                    bias = nn.zeros(bias_length)
+                    bias = np.zeros(bias_length)
                     i += 1
                 nn.numfilters.append(filters.shape[3])
                 nn.filter_size.append([filters.shape[0], filters.shape[1]])
@@ -475,7 +475,7 @@ class Optimizer:
                     matrix, input_names, output_name, b_output_shape = self.resources[i][domain]
 
                     bias_length = reduce((lambda x, y: x * y), b_output_shape)
-                    bias = nn.zeros(bias_length)
+                    bias = np.zeros(bias_length)
 
                     i += 1
                 # print("type ", type(matrix), type(bias), matrix.dtype, bias.dtype)
